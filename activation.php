038c89abea87cb908edd76f9cca9f9166830200e
Marc-Antoine Gelinas <6138513@cegepat.qc.ca>
2018-05-14 17:11:44 -0400

    CSS + gestion nom classeur

--- activation.php
+++ activation.php
@@ -10,68 +10,68 @@ include_once 'Include/genererSession.include.php';
     <title>Key Vault - Activation</title>
     <link rel="stylesheet" type="text/css" href="CSS/main.css">
     <link rel="stylesheet" type="text/css" href="CSS/grid.css">
+
 </head>
 <body class="main-Grid">
-    <div id="headerL">
-        <a href="Redirect/logoClick.redirect.php" class="logo"><img src="Ressources/Logo.png"></a>
-    </div>
+<div class="headerL">
+    <a style="font-family: impact; font-size:larger;">Keyvault</a>
+</div>
 
-    <div id="headerC">
-    </div>
+<div class="headerC">
 
-    <div id="headerR">
-    </div>
+</div>
 
+<div class="headerR">
 
-        <?php
-        if (isset($_SESSION['Notification'])) {
-            echo "<div id='bodyL' class='section'>";
-            echo '<h1 style="Color:red">' . $_SESSION['Notification'] . '</h1>';
-            unset($_SESSION['Notification']);
-            echo "</div>";
-        }
-        else
-            echo "<div id='bodyL'>";
-            echo "</div>";
-        ?>
+</div>
 
+<div class="bodyL">
+    <?php
+    if (isset($_SESSION['Notification'])) {
+        echo '<h1 style="Color:red">' . $_SESSION['Notification'] . '</h1>';
+        unset($_SESSION['Notification']);
+    }
+    ?>
+</div>
 
-    <div id="bodyC" class="section centerDefault">
-        <Form name="formActivation" method="post" action="Redirect/validerActivation.redirect.php">
-            <p class="titre">Entrer vos informations d'activation</p>
-            <label for="email">Adresse e-mail :
-                <input type="text" name="email" id="email">
-            </label>
-            <label for="pass">Mot de passe :
-                <input type="password" name="pass" id="pass">
-            </label>
-            <label for="code">Code d'activation :
-                <input type="text" name="code" id="code">
-            </label>
-            <p class="titre">Veuillez saisir une question de sécurité pour validé votre identité en cas de perte de mot de passe.</p>
-            <label for="question">Question de sécurité :
-                <input type="text" name="question" id="question">
-            </label>
-            <label for="reponse">Réponse :
-                <input type="text" name="reponse" id="reponse">
-            </label>
-            <button onclick="formActivation.submit()">Activer</button>
-        </Form>
-    </div>
+<div class="bodyC">
+    <Form name="formActivation" method="post" action="Redirect/validerActivation.redirect.php">
+        <label for="email">Adresse e-mail :
+            <input type="text" name="email" id="email">
+        </label>
+        <label for="pass">Mot de passe :
+            <input type="password" name="pass" id="pass">
+        </label>
+        <label for="code">Code d'activation :
+            <input type="text" name="code" id="code">
+        </label>
+        <br/>
+        <br/>
+        <h3>Veuillez saisir une question de sécurité pour validé votre identité en cas de perte de mot de passe.</h3>
+        <label for="question">Question de sécurité :
+            <input type="text" name="question" id="question">
+        </label>
+        <label for="reponse">Réponse :
+            <input type="text" name="reponse" id="reponse">
+        </label>
+        <button onclick="formActivation.submit()">Activer</button>
+    </Form>
+</div>
 
-    <div id="bodyR">
-    </div>
+<div class="bodyR">
 
-    <div id="footerL">
+</div>
 
-    </div>
+<div class="footerL">
 
-    <div id="footerC">
-        <a>Fait par Marc-Antoine Gélinas</a>
-        <a>Dans le cadre du cours Projet Web 2018</a>
-    </div>
+</div>
 
-    <div id="footerR">
-    </div>
+<div class="footerC">
+
+</div>
+
+<div class="footerR">
+
+</div>
 </body>
 </html>
